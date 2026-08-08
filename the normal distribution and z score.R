f2787564d5584d40776ebbff8d5d8d008c4f0d3c
Md. Rashedul islam <www.rashedmd234@gmail.com>
2026-08-09 00:11:16 +0600

    day 4

--- the normal distribution and z score.R
+++ the normal distribution and z score.R
@@ -1,7 +1,8 @@
-setwd("C:/Rashed/R programming/r git hub/mastering-statistics")
 library(tidyverse)
 library(titanic)
 library(moments)
 #mean and sd of the age
 mean_age<-mean(titanic_train$Age,na.rm = TRUE)
 sd_age<-sd(titanic_train$Age,na.rm = TRUE)
+
+#Z score
